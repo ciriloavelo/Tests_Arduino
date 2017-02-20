ef233a00f454912f638e007c2f9749a35e2213d3
ciriloavelo <cyril.romagnoli@gmx.com>
2017-02-20 19:53:53 +0100

    Ajout Prg Arduino mesures T & H sur serveur web

---
+++ DHTEthernetCRO.ino
@@ -0,0 +1,153 @@
+#include <DHT.h>
+#include <SPI.h>
+#include <Ethernet2.h>
+
+/*
+  Web Server
+
+ A simple web server that shows the value of the analog input pins.
+ using an Arduino Wiznet Ethernet shield.
+
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13
+ * Analog inputs attached to pins A0 through A5 (optional)
+
+ created 18 Dec 2009
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe
+ modified 02 Sept 2015
+ by Arturo Guadalupi
+
+ */
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {0x90, 0xA2, 0xDA, 0x10, 0xDE, 0x1C};
+byte ip[] = {192, 168, 0, 25};
+//IPAddress ip(192,168,0,25);
+//byte gateway[] = {192,168,0,254};
+//IPAddress gateway(192,168,0,254);
+//byte submak[] = {255,255,255,0};
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetClient client;
+EthernetServer server(80);
+
+#define DHTPIN A0     // what pin we're connected to
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11 
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  // start the serial library:
+  Serial.begin(9600);
+  // start the Ethernet connection:
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    for(;;)
+      ;
+  }
+  // print your local IP address:
+  Serial.print("My IP address: ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    // print the value of each byte of the IP address:
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print("."); 
+  }
+  Serial.println();
+}
+//void setup() {
+//  // Open serial communications and wait for port to open:
+//  Serial.begin(9600);
+//  while (!Serial) {
+//    ; // wait for serial port to connect. Needed for native USB port only
+//  }
+//
+//
+//  // start the Ethernet connection and the server:
+//  Ethernet.begin(mac, ip, gateway);
+//  server.begin();
+//  Serial.print("server is at ");
+//  Serial.println(Ethernet.localIP());
+//}
+
+
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+  
+    // output the value of each analog input pin  
+  // Reading temperature or humidity takes about 250 milliseconds!
+      // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+      float h = dht.readHumidity();
+      float t = dht.readTemperature();
+
+      // check if returns are valid, if they are NaN (not a number) then something went wrong!
+      if (isnan(t) || isnan(h))
+    {
+      client.println("Failed to read from DHT");
+    }
+    else 
+        {
+          client.print("Humidity: "); 
+          client.print(h);
+          client.print(" %\t");
+          client.print("Temperature: "); 
+          client.print(t);
+          client.print(" *C");
+          client.println("<br />");
+        }
+
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
+
--- README.txt
+++ README.txt
@@ -9,3 +9,8 @@ SW Mise en forme et écriture des 2 mesures analogiques sur la console.
 WebServer.ino
 HW Arduino Uno R3, Ethernet shield v2
 Configuration de l'adresse MAC et forçage de l'adresse IP 
+
+3eme essai
+DHTEthernetCRO.ino
+HW Arduino Uno R3, Ethernet shield v2, Grove base shield, Temp and Moisture sensor pro
+Lecture de la mesure de température et humidité à l'adresse http://192.168.0.25
